050a42a1081a85d1bba2db94fe9917d6f2c19548
melnikovmaks <maksim9805@mail.ru>
2022-02-15 18:35:29 +0300

    scripts

---
+++ scripts/selenoid-and-run-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker pull selenoid/chrome
+chmod +x selenoid_manager/cm
+selenoid_manager/cm selenoid start
+selenoid_manager/cm selenoid status
+curl http://localhost:4444/status
+mvn clean test
